e5e1bd2233f9204c92e250fc6c07ac2e4578f897
KOMAL <sharma3.komal@gmail.com>
2023-04-23 17:37:04 +0530

    first commit
    first commit

---
+++ GharKiKheti/src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/paddy.png";
+import product2 from "./assets/products/maize.png";
+import product3 from "./assets/products/jowar.png";
+import product4 from "./assets/products/moong.png";
+import product5 from "./assets/products/bajra.png";
+import product6 from "./assets/products/barley.png";
+import product7 from "./assets/products/cotton.png";
+import product8 from "./assets/products/groundnut.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Paddy",
+    price: 25,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Maize",
+    price: 22,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Jowar",
+    price: 35,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Moong",
+    price: 80,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Bajra",
+    price: 29,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Barley",
+    price: 23,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Cotton",
+    price: 65,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Groundnut",
+    price: 62,
+    productImage: product8,
+  },
+];
